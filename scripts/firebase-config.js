d5842b98fda7ea30eba71e80ecfeedb09a2d76a2
somjairs8542-pixel <somjairs8542@gmail.com>
2025-08-04 11:46:58 +0700

    Create firebase-config.js

---
+++ scripts/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCaaZ589IXIpZGyM4SQO3fM_P9fwMbgFvQ",
+  authDomain: "ruaylotto888.firebaseapp.com",
+  projectId: "ruaylotto888",
+  storageBucket: "ruaylotto888.firebasestorage.app",
+  messagingSenderId: "25251793687",
+  appId: "1:25251793687:web:1f1079aed66b0600bdc552",
+  measurementId: "G-RD2V30L7ME"
+};
